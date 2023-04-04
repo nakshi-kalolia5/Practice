314dd17a242e7d25f91a51d7d6e95dc5d8fc8d40
Shaktiraj <shaktirajsinh.zala@improwised.com>
2023-04-04 11:52:39 +0530

    objects

--- functions.js
+++ functions.js
@@ -87,3 +87,49 @@
 // }
 
 // console.log(ncr(5,2, factorial));
+
+// * Array
+// var arr = [1,2,3,4]
+// or
+// var arr = new Array(1,2,3)
+// console.log(arr);
+// arr[2] = 5
+
+// console.log(arr.length);
+// to specify the size  var arr2 = new Array(2)
+// push and pop also work with array
+// arr.shift() shifts the array element left and the first element will be removed. it returns the removed element
+// arr.unshift(val) val is the input that will be at first place, and all elements will be shifted right
+
+// * Array can be hetrogeneous
+// var newarr = [12, "hi"]
+
+// * Splice function in array
+// used to make changes(addition,removal,.. of n elemets) to array
+// takes 3 type of arguments
+// 1. Starting index
+// 2. Deletion count
+// 3. elements to be inserted (optional)
+
+// var arr = [2,3,4,5,6]
+// arr.splice(1,1) //removes 3
+
+// arr.splice(1) //delete all elements
+
+// arr.splice(2,0,10) //10 will be at index 2
+
+// both operations can be performed parallely
+// arr.splice(2,2,40,50) // 2 elements from index 2 wil be deleted and 40, 50 added at that position
+
+// * Iterate over array
+// using simple for loop by iterating all indices
+// using foreach
+var arr = [2,3,4,5,6,7]
+
+function print(element) { // optional args index, array 
+    console.log(element);
+}
+
+arr.forEach(print) //pass function as argument, that will be executed for each element
+// don't have to pass argument element
+// optional args : index and whole array
